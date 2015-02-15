e83607f32f85be1aedfe6310b28c0e232107d59b
Jacob Lane <lane.jacob@gmail.com>
2015-02-14 21:22:33 -0500

    tweak

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ var gutil = require('gulp-util')
     , pdf = require('html-pdf');
 
 module.exports = function (options) {
-	return through.obj(function (file, enc, cb) {
-		if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      cb(null, file);
+      return;
+    }
 
-		if (file.isStream()) {
-			cb(new gutil.PluginError('gulp-html-pdf', 'Streaming not supported'));
-			return;
-		}
+    if (file.isStream()) {
+      cb(new gutil.PluginError('gulp-html-pdf', 'Streaming not supported'));
+      return;
+    }
 
-		pdf.create(file.contents.toString(), options)
-  		.toBuffer(function (err, buffer) {
-  			if (err) {
-  				cb(new gutil.PluginError('gulp-html-pdf', err, {fileName: file.path}));
-  				return;
-  			}
+    pdf.create(file.contents.toString(), options)
+      .toBuffer(function (err, buffer) {
+        if (err) {
+          cb(new gutil.PluginError('gulp-html-pdf', err, {fileName: file.path}));
+          return;
+        }
 
-  			file.contents = buffer;
-  			file.path = gutil.replaceExtension(file.path, '.pdf');
-  			cb(null, file);
-		  });
-	});
+        file.contents = buffer;
+        file.path = gutil.replaceExtension(file.path, '.pdf');
+        cb(null, file);
+      });
+  });
 };
